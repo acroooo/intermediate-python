cd3659318af4e435337fd14193bea2a58810cc1e
Hernan <hernan@mukiku.com>
2022-02-08 23:47:49 -0300

    'clean-solution-challenge-try-except'

--- debugging.py
+++ debugging.py
@@ -1,17 +1,23 @@
 def divisors(num):
     divisors = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            divisors.append(i)
-    return divisors
+    try:
+        if num < 0:
+            raise ValueError("Ingresa un número positivo")
+        for i in range(1, num + 1):
+            if num % i == 0:
+                divisors.append(i)
+        return divisors
+    except ValueError as err:
+        print(err)
 
 
 def run():
     try:
         num = int(input("Ingresa un número: "))
-        print(divisors(num))
-        print("Fin programa")
-    except ValueError:
+        result = divisors(num)
+        if result is not None:
+            print("Los divisores de {} son: {}".format(num, result))
+    except ValueError as ve:
         print("Se debe ingresar un número")
 
 
